178a07a999e9d8c54a56c14f5ea560c50fcbc31c
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-10-25 13:57:19 +0200

    fix circular dependency

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-from db import db
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.user import UserRegister
@@ -14,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'kostis'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, '/item/<string:name>')
@@ -27,5 +22,4 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
